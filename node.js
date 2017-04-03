e494be607b2f938d863f9a82b00d44256a8a24e6
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2017-04-03 23:36:43 +0530

    Routing functionality added

--- node.js
+++ node.js
@@ -5,6 +5,18 @@ app.get('/',function(req,res) {
 	res.send('Server has been set up');
 })
 
+app.post('/',function(req,res) {
+	res.send('post request encountered');
+})
+
+app.put('/user',function(req,res) {
+	res.send('put request encountered');
+})
+
+app.get('/user',function(req,res) {
+	res.send('delete request encountered');
+})
+
 app.listen(3000,function(){
 	console.log('Listening to port 3000');
 })
